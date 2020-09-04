6e5d367ef09895fcba09420b32675492643d2aa4
bobwmcgrath <bobwmcgrath@gmail.com>
2020-09-04 07:54:18 -0500

    working example

--- app/src/main/java/com/example/viewvideo/MainActivity.java
+++ app/src/main/java/com/example/viewvideo/MainActivity.java
@@ -22,15 +22,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //assign video
-        mVideosListView = (ListView) findViewById(R.id.videoListView);
+        mVideosListView = findViewById(R.id.videoListView);
 
         //create videos
         Video riverVideo = new Video("https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8"); //testing
-        //Video riverVideo = new Video("rtmp://192.168.1.3/webcam/mystream");
-        //Video riverVideo = new Video("rtsp://192.168.1.10:8554/");
         //Video riverVideo = new Video("http://192.168.1.6/hls/index.m3u8"); //working
-        //Video riverVideo = new Video("http://71.57.126.1/hls/index.m3u8");
-        //Video riverVideo = new Video("udp://192.168.1.6:5000");
+        //Video riverVideo = new Video("http://proxy21.rt3.io:38060/hls/index.m3u8");//remot.it
 
         mVideosList.add(riverVideo);
 
